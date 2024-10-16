7caa0c865404e5953bac02d2069fcc83a525c432
Nour Al Salhi <nuraym9998@gmail.com>
2024-10-16 11:41:30 +0300

    create next provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
 import Navbar from "./components/Navbar";
-import localFont from "next/font/local";
+import AuthProvider from "./context/AuthProvider";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,13 +15,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Navbar />
-        <main className="flex justify-center items-start p-6 min-h-screen">
-          {children}
-        </main>
+      <body>
+        <AuthProvider>
+          <Navbar />
+          <main className="flex justify-center items-start p-6 min-h-screen">
+            {children}
+          </main>
+        </AuthProvider>
       </body>
     </html>
   );
